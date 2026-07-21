f86af7a33dc82efd7e3c97f92c78e4022b29337c
Nate Herman <NateWHerman05@gmail.com>
2026-07-20 22:59:34 -0500

    added chat page with conversation panel

--- Front-end/src/Chat.jsx
+++ Front-end/src/Chat.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import "./Chat.css"
+
+
 export default function Chat() {
-    return (
-        <div className="chat-page">
-            <h1>Messaging App</h1>
-        </div>
-    );
+    const conversation = [
+        {
+        id: 1,
+        name: "Sarah",
+        lastMessage: "Are we still on for tmr?"
+        },
+        {
+        id: 2,
+        name: "Alex",
+        lastMessage: "That works for me"
+        },
+        {
+        id: 3,
+        name: "Group Study",
+        lastMessage: "I uploaded the notes"
+        },
+        
+    ]
 }
