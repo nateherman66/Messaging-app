e0304c18edf997bf505d6f293e14b395681280a7
Nate Herman <NateWHerman05@gmail.com>
2026-07-21 18:39:05 -0500

    made chat page with panel connected with the css page

--- Front-end/src/Chat.jsx
+++ Front-end/src/Chat.jsx
@@ -3,7 +3,7 @@ import "./Chat.css"
 
 
 export default function Chat() {
-    const conversation = [
+   const conversations = [
         {
         id: 1,
         name: "Sarah",
@@ -19,6 +19,47 @@ export default function Chat() {
         name: "Group Study",
         lastMessage: "I uploaded the notes"
         },
-        
-    ]
+    ];
+
+    const [selectedChat, setSelectedChat] = useState(conversations[0]);
+
+    return (
+        <div className = "chat-page">
+            <aside className = "chat-sidebar">
+                <h2>Chats</h2>
+
+                {conversations.map((conversations) => (
+                <button
+                key = {conversations.id}
+                type = "button"
+                className = "chat-list-item"
+                onClick = {() => setSelectedChat(conversations)}
+                >
+                    <strong>{conversations.name}</strong>
+                    <span>{conversations.lastMessage}</span>
+                </button>
+                ))}
+            </aside>
+
+
+            <main className="chat-main">
+                <header className="chat-header">
+                    <h2>{selectedChat.name}</h2>
+                </header>
+
+                <div className="messages">
+                    <div className="received-message">Hey! How are you?</div>
+                     <div className="sent-message">I'm doing well!</div>
+                     <div className="received-message">
+                        {selectedChat.lastMessage}
+                     </div>
+                    </div>
+
+                    <form className="message-form">
+                        <input type="text" placeholder="Message" />
+                        <button type="submit">Send</button>
+                    </form>
+            </main>
+        </div>
+    )
 }
